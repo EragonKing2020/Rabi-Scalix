1d7b6be90e87df2378ba873c00c5729e07a9ca48
EragonKing2020 <Eragonking2020@gmail.com>
2024-11-27 16:31:22 +0100

    key

--- src/main/scala/scalix/Scalix.scala
+++ src/main/scala/scalix/Scalix.scala
@@ -3,7 +3,7 @@ package scalix
 import org.json4s.native.JsonMethods._
 
 object Scalix extends App:
-    val key = ""
+    val key = "1f1a29a9202654e114671efe1078f4e7"
     val url = s""
     val source = scala.io.Source.fromURL(url)
     val contents = source.mkString
