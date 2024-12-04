32fc166b934ae118bcbfc63aaaa5402f61e813e4
EragonKing2020 <Eragonking2020@gmail.com>
2024-12-04 15:25:53 +0100

    Yo les bros

--- src/main/scala/scalix/Scalix.scala
+++ src/main/scala/scalix/Scalix.scala
@@ -1,18 +1,45 @@
 package scalix
 
+import org.json4s._
 import org.json4s.native.JsonMethods._
 
-class Scalix extends App:
+class Scalix:
     def key = "1f1a29a9202654e114671efe1078f4e7"
-    def url = s"https://api.themoviedb.org/3/search/"
-    def endUrl = s"&api_key=$key"
+    def url = s"https://api.themoviedb.org/3/"
+    def endUrl = s"api_key=$key"
     def findActorId(name: String, surname: String): Option[Int] = Some(3)
 
-object Scalix:
-    val key = "1f1a29a9202654e114671efe1078f4e7"
+    def findActorMovies(actorId : Int): Set[(Int, String)] = {
+        val source = scala.io.Source.fromURL(url + s"person/$actorId/movie_credits?" + endUrl)
+        val json = parse(source.mkString)
+        val tbl = for {
+            case JObject(o) <- json
+            case JField("original_title", JString(title)) <- o
+            case JField("id", JInt(id)) <- o
+        } yield (id, title)
+        return tbl.map((id, title) => (id.toInt ,title)).toSet
+    }
+
+    def findMovieDirector(movieId: Int): Option[(Int, String)] = {
+        val source = scala.io.Source.fromURL(url + s"movie/$movieId/credits?" + endUrl)
+        val json = parse(source.mkString)
+        val tbl = for {
+            case JObject(o) <- json
+            case JField("job", JString("Director")) <- o
+            case JField("id", JInt(id)) <- o
+            case JField("name", JString(name)) <- o
+        } yield (id.toInt, name)
+        return Option.when(tbl.nonEmpty)(tbl.head)
+    }
+
+object Scalix extends App:
+    val test = Scalix()
+    println(test.findActorMovies(1))
+    println(test.findMovieDirector(10000))
+    /*val key = "1f1a29a9202654e114671efe1078f4e7"
     val url = s"https://api.themoviedb.org/3/search/person?query=Tom%20Cruise&api_key=$key"
     val source = scala.io.Source.fromURL(url)
     val contents = source.mkString
-    println(contents)
+    println(contents)*/
     //val json = parse(contents)
     //println(json)
