a0dfc02a90d63c201370ecc707c05e72ecfd071d
Quentin Rabilloud <quentin.rabilloud@imt-atlantique.net>
2024-12-04 16:28:58 +0100

    Added collaboration

--- src/main/scala/scalix/Scalix.scala
+++ src/main/scala/scalix/Scalix.scala
@@ -1,7 +1,7 @@
 package scalix
 
-import org.json4s._
-import org.json4s.native.JsonMethods._
+import org.json4s.*
+import org.json4s.native.JsonMethods.*
 
 case class FullName(name: String, surname: String)
 
@@ -9,22 +9,19 @@ class Scalix:
     def key = "1f1a29a9202654e114671efe1078f4e7"
     def url = s"https://api.themoviedb.org/3/"
     def endUrl = s"api_key=$key"
-    def getJsonFromUrl(url: String): Option[String] =
+
+    def getParsedDataFromActor(actor: FullName): Option[JValue] =
         try {
-            println(url)
-            Some(scala.io.Source.fromURL(url).mkString)
+            Some(parse(scala.io.Source.fromURL(url + "search/person?query=" + actor.name + "%20" + actor.surname + "&" + endUrl).mkString))
         } catch {
             case e: Exception => None
         }
 
     def findActorId(name: String, surname: String): Option[Int] =
-        val request = url + "search/person?query=" + name + "%20" + surname + "&" + endUrl
-        println(request)
-        getJsonFromUrl(request) match
+        getParsedDataFromActor(FullName(name,surname)) match
             case Some(actorInfo) => {
-                val parsedActorInfo = parse(actorInfo)
                 val ids = for {
-                    case JObject(o) <- parsedActorInfo
+                    case JObject(o) <- actorInfo
                     case JField("id", JInt(id)) <- o
                 } yield id
                 ids match
@@ -59,15 +56,33 @@ class Scalix:
         return Option.when(tbl.nonEmpty)(tbl.head)
     }
 
+    def collaboration(actor1: FullName, actor2: FullName): Set[(String, String)] =
+        (findActorId(actor1.name,actor1.surname),findActorId(actor2.name,actor2.surname)) match
+            case (Some(id1),Some(id2)) => {
+                val movies1 = for {
+                    case (id, name) <- findActorMovies(id1)
+                } yield (id, name)
+                val commonMovies = for {
+                    case (id, name) <- findActorMovies(id2) if movies1.contains((id, name))
+                } yield (id, name)
+                commonMovies.map((id,name) => name).zip(commonMovies.map((id,name) => findMovieDirector(id) match
+                    case Some(directorId, directorName) => directorName
+                    case None => "No director specified.")
+                )
+            }
+            case _ => Set()
+
+
 object Scalix extends App:
     val test = Scalix()
     println(test.findActorId("Tom", "Cruise"))
     println(test.findActorMovies(1))
     println(test.findMovieDirector(10000))
+    println(test.collaboration(FullName("Tom","Cruise"), FullName("Emily","Blunt")))
     /*val key = "1f1a29a9202654e114671efe1078f4e7"
     val url = s"https://api.themoviedb.org/3/search/person?query=Tom%20Cruise&api_key=$key"
     val source = scala.io.Source.fromURL(url)
     val contents = source.mkString
-    println(contents)*/
-    //val json = parse(contents)
-    //println(json) 
+    println(contents)
+    val json = parse(contents)
+    println(json)*/
