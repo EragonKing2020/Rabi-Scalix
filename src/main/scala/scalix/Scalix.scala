76d178bdd5c7d0c54849f72899806235f77e878c
Quentin Rabilloud <quentin.rabilloud@imt-atlantique.net>
2024-12-04 14:31:48 +0100

    Base code

--- src/main/scala/scalix/Scalix.scala
+++ src/main/scala/scalix/Scalix.scala
@@ -2,11 +2,17 @@ package scalix
 
 import org.json4s.native.JsonMethods._
 
-object Scalix extends App:
+class Scalix extends App:
+    def key = "1f1a29a9202654e114671efe1078f4e7"
+    def url = s"https://api.themoviedb.org/3/search/"
+    def endUrl = s"&api_key=$key"
+    def findActorId(name: String, surname: String): Option[Int] = Some(3)
+
+object Scalix:
     val key = "1f1a29a9202654e114671efe1078f4e7"
-    val url = s""
+    val url = s"https://api.themoviedb.org/3/search/person?query=Tom%20Cruise&api_key=$key"
     val source = scala.io.Source.fromURL(url)
     val contents = source.mkString
     println(contents)
-    val json = parse(contents)
-    println(json)
+    //val json = parse(contents)
+    //println(json)
